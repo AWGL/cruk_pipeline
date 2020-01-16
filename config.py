627bd714bfdd4808391fba414e752c3b72a2cc1c
Sara Rey <sara.rey.manchester@gmail.com>
2020-01-16 14:00:41 +0000

    increment SMP2 app version

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 app_name = "TruSight Tumor 170"
 app_version = "1.0.3"
 smp2_app_name = "SMP2 v3"
-smp2_app_version = "1.1.1851"
+smp2_app_version = "1.1.1858"
 v1_api = "https://api.euc1.sh.basespace.illumina.com/v1pre3" #"https://api.basespace.illumina.com/v1pre3"
 v2_api = "https://api.euc1.sh.basespace.illumina.com/v2" #"https://api.basespace.illumina.com/v2"
 download_file_extensions = ["xlsm", "bam", "bai"]
